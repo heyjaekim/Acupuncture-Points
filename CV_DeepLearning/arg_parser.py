3543f634bb118675f5f1d7b12335781e6a86ddbb
Jungkyu <normal.kim12@gmail.com>
2020-08-31 02:51:12 +0900

    Updated main file and parser

--- CV_DeepLearning/arg_parser.py
+++ CV_DeepLearning/arg_parser.py
@@ -8,12 +8,23 @@ def arg_parser():
 def common_arg_parser():
     parser = arg_parser()
 
+    parser.add_argument('--kw', default= 'hapgok')
     parser.add_argument('--data', metavar='DIR', help='path to dataset')
-    parser.add_argument('--prefix', default = 'Hi', type = str, help = 'prefix for logging & ckpt save')
+    parser.add_argument('--prefix', default = '', type = str, help = 'prefix for logging & ckpt save')
     parser.add_argument('--model', default='resnet34', type=str, help='Model Architecture (default: resnet34)')
-    parser.add_argument('--epochs', default=10, type=int, metavar='N', help='number of total epochs to run')
+    parser.add_argument('--epochs', default=80, type=int, metavar='N', help='number of total epochs to run')
     parser.add_argument('--start_epoch', default=0, type=int, metavar='N', help='starting epoch')
     parser.add_argument('--workers', default=4, type=int, metavar='N', help='number of data loading workers (default: 4)')
     parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
+    parser.add_argument('--test_ratio', type = float, default = '0.01')
+    parser.add_argument('--valid_ratio', type = float, default = '0.1')  
 
+    parser.add_argument('--bs', type = int, default= '32')
+    parser.add_argument('--lr', type = float, default= '0.001')
+    parser.add_argument('--loss_type', default = 'mse')
+    parser.add_argument('--rescale', type = int, default = '256')
+
+    parser.add_argument('--decay', type = float, default = '0.9')
+    parser.add_argument('--decay_step', type = int, default= 1)
+    parser.add_argument('--betas', type = float, default= (0.5, 0.999) )
     return parser
--- CV_DeepLearning/main.py
+++ CV_DeepLearning/main.py
@@ -1,54 +1,143 @@
-import os
+import Training_utils, torchvision
+from torchvision import transforms
+import tb_utils, torch, model_utils, sys, os
+from tb_utils import *
+from Training_utils import *
 from Image_Process_utils import *
+import model_utils
 from model_utils import *
 from Dataset_utils import *
-from Training_utils import *
+import arg_parser
 from arg_parser import common_arg_parser
-from tb_utils import *
+from torchsummary import summary
 
 if not os.path.exists('./checkpoints'):
     os.mkdir('./checkpoints')
 
-best_val = 100000000
+global t_step
+global v_step
+global test_step
+global micro_step
+global v_micro
+
+def today_timeinfo():
+    today = str(datetime.datetime.now().today())
+    return today[5:7] +today[8:10] + '_' + today[11:13] + today[14:16]
+
+def print_dash():
+    print('##################################################################')
+
 
 def main(args):
+    # Parser
     arg_parser = common_arg_parser()
     args, unknown_args = arg_parser.parse_known_args()
-    model = create_model(args.model)
-    #summary(model.to('cuda'), input_size = (3, 700, 700))
+    args.prefix = args.kw + today_timeinfo() + args.prefix
 
-    # Load dataset 
-    img_dir = './Acu_Dataset/sotack/org'
-    json_file = './Acu_Dataset/sotack/sotack_info.json'
+    # Initialize  
+    print('\n\nInitializing...\n')
+    best_val = 100000000
+    t_step = v_step = test_step = micro_step = v_micro = 0
+    test_imlist, target_imlist =  gen_test_img_list()
 
+    print(f'Checkpoint Name: {args.prefix}')
+    print(f'Training keyword : {args.kw}')
+    print(f'Current Path:{os.getcwd()} \n')
+
+    print_dash()
+    print('\n<Hyperparameters>')
+    print('0. Model :', args.model)
+    print('1.Batch_size :', args.bs)
+    print('2.Learning rate: ', args.lr)
+    print('3.Rescale: ', args.rescale)
+    print(f'4.Schedule_Decay: Decaying {args.decay} per {args.decay_step} step')
+    print(f'5. Epoch : {args.epochs}\n')
+    print_dash()
+
+    print('\nCreating Dataset...\n')
+    
+    # Transformation 
     my_transforms = transforms.Compose([
-    #transforms.Normalize((0.5,), (0.5,)),
-    Rescale(256),
-    Tensorize()
-    ])
-    sotack_dataset = HandDataSet(json_file, img_dir, transform = my_transforms, train=True) # pytorch 로 변환
+        #transforms.Normalize((0.5,), (0.5,)),
+        Rescale(args.rescale),Tensorize()])
 
-    # split dataset
+    # Dataset   
+    concat_dataset = concat_augmented(kw = args.kw, transform = my_transforms)   
+    train_set, valid_set, test_set = train_test_valid_splitter(concat_dataset, args.test_ratio, args.valid_ratio)
 
+    # Data Loader
+    train_loader = DataLoader(train_set, batch_size = args.bs, shuffle = True, num_workers=args.workers)
+    valid_loader = DataLoader(valid_set, batch_size = args.bs, shuffle = True, num_workers=args.workers)
+    test_loader = DataLoader(test_set, batch_size = args.bs, shuffle = True, num_workers=args.workers)
 
+    # Create Model
+    model = create_model(args.model)
 
+    # GPU Compatibility
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print('Running on: ', device) 
+
+    if device != 'cpu':
+        model.to('cuda')
+        #summary(model.to('cuda'), input_size = (3, args.rescale, args.rescale))
+        pass
+    
+    # optimizer and Scheduler 
+    optimizer = torch.optim.Adam(model.parameters(), lr = args.lr, betas = args.betas)
+    scheduler = StepLR(optimizer, step_size = args.decay_step, gamma = args.decay)
+    
+    # loss function
+    cls_loss = class_loss()
+    crd_loss = coord_loss(kw = args.loss_type)
+
+    # Tensorboard Writer
+    writer = SummaryWriter('runs/'+args.prefix)
+    
+
+    # Resume from best results
     if args.resume:
         if os.path.isfile(args.resume):
             print("=> loading checkpoint '{}'".format(args.resume))
             checkpoint = torch.load(args.resume)
             args.start_epoch = checkpoint['epoch']
-            best_val = checkpoint['best_prec1']
+            best_val = checkpoint['best_val']
             model.load_state_dict(checkpoint['state_dict'])
             if 'optimizer' in checkpoint:
                 optimizer.load_state_dict(checkpoint['optimizer'])
             print("=> loaded checkpoint '{}' (epoch {})"
-                  .format(args.resume, checkpoint['epoch']))
+                .format(args.resume, checkpoint['epoch']))
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
 
-    
 
-    pass
+    # train loop
+    for epoch in range(args.start_epoch, args.epochs):
+        _, micro_step, t_step =  train(model, train_loader, optimizer, crd_loss, cls_loss, epoch, writer, micro_step, t_step)
+        val_loss, v_micro, v_step = validate(model, valid_loader, crd_loss, cls_loss, epoch, writer, v_micro, v_step)
+        test_step = test(test_imlist, target_imlist, model, writer, 'test_image', test_step)
+        writer.flush()
+
+        is_best = sum(val_loss[0:2]) < best_val
+        best_val = min( sum(val_loss[0:2]), best_val )
+
+        save_checkpoint({
+        'epoch' : epoch,
+        'model' : args.model, 
+        'state_dict' : model.state_dict(),
+        'best_val' : best_val, 
+        'optimizer' : optimizer.state_dict(),
+        'scheduler' : scheduler.state_dict()
+        }, is_best, args.prefix)
+    
+        scheduler.step()
+
+    args.resume = './checkpoints/ekmoon0830_2236all+sc+sc_filled_model_best.pth.tar'
+
+    # summary of results
+    print_dash()
+    print(f'Training DONE on epoch: {epoch}')
+    print(f'Best Validation Loss: {best_val}')
+
 
 if __name__ == '__main__':
     main(sys.argv)
